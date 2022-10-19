be21d7e74c74f29fa86edb3e2003abb10690ed4d
mmyrberg <m.myrberg@outlook.com>
2022-10-19 18:02:19 +0200

    Questions answerd

--- s80_klasser/s60_enkel_klass/bil.py
+++ s80_klasser/s60_enkel_klass/bil.py
@@ -3,3 +3,10 @@
 # modell
 # mil
 # topphastighet
+
+class Bil:
+    def __init__(self, märke, modell, mil, topphastighet):
+        self.märke = märke
+        self.modell = modell
+        self.mil = mil
+        self.topphastighet = topphastighet
--- s80_klasser/s70_arv/åke/arv.py
+++ s80_klasser/s70_arv/åke/arv.py
@@ -4,3 +4,16 @@
 #Aborre ärver från Fisk
 #Fisk ska ha attributet simhastighet som sätts i konstruktorn
 #Andra fiskar ska också skapas med simhastighet som argument
+
+class Fisk:
+    def __init__(self, simhastighet):
+        self.simhastighet = simhastighet
+
+class Gädda(Fisk):
+    def __init__(self, simhastighet):
+        super().__init__(simhastighet)
+
+class Aborre(Fisk):
+    def __init__(self, simhastighet):
+        super().__init__(simhastighet)
+
